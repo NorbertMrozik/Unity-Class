36588039541dcd6342637b58514fb3ccd821ecf4
Norbert <norbertmrozik@gmail.com>
2022-11-15 12:56:48 +0000

    Week 7 Unity

--- Assets/SphereColission.cs
+++ Assets/SphereColission.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SphereColission : MonoBehaviour
 {
-
+    public TMP_Text scoreText;
+    private int score;
     public GameObject coin;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem coinParticle;
+    public Transform ballPos;
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.CompareTag("Coin"))
         {
             col.gameObject.SetActive(false);
+            score++;
+            scoreText.text = "Score: " + score;
+            Instantiate(coinParticle, transform.position, transform.rotation);
         }
     }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.CompareTag("Plane"))
+        {
+            Debug.Log("Plane collided");
+            Instantiate(explosionParticle, transform.position, transform.rotation);
+        }
+    }
+
+
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -24,6 +43,8 @@ public class SphereColission : MonoBehaviour
             Instantiate(coin, objectPos, Quaternion.identity);
         }
     }
+    
+    
 
 
 }
